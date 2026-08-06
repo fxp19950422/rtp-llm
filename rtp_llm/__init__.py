c64c03491eacec03e4abbc3db5e448a3db2dc463
元翼 <fanxingpeng.fxp@alibaba-inc.com>
2026-08-06 17:47:08 +0800

    feat(startup): warm serving shapes before publishing health

--- rtp_llm/__init__.py
+++ rtp_llm/__init__.py
@@ -10,17 +10,11 @@ except ImportError:
 # Import internal models to register them
 from rtp_llm.utils.import_util import has_internal_source
 from rtp_llm.utils.torch_patch import *
-from rtp_llm.utils.triton_compile_patch import enable_compile_monitor
+from rtp_llm.utils.triton_compile_patch import maybe_enable_compile_monitor
+
+maybe_enable_compile_monitor()
 
 from .ops import *
-
-# check triton version
-# if triton.__version__ < "3.4":
-#     enable_compile_monitor()
-
-
-# enable_compile_monitor()
-
 
 if has_internal_source():
     import internal_source.rtp_llm.models_py
