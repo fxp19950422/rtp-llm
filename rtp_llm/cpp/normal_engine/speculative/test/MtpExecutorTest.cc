a5561b8a027b2ff192adc6ce74c47a55c2e020bb
fanxingpeng.fxp <fanxingpeng.fxp@alibaba-inc.com>
2026-08-28 04:18:30 +0800

    Revert "test: assert numerical failure stream error"
    This reverts commit 47ad7251fc5f887a880e7e0063229f2bdc905394.

--- rtp_llm/cpp/normal_engine/speculative/test/MtpExecutorTest.cc
+++ rtp_llm/cpp/normal_engine/speculative/test/MtpExecutorTest.cc
@@ -802,8 +802,7 @@ TEST_F(MtpExecutorTest, testForceDisableNumericalFailureDoesNotAcceptToken) {
     auto status = components.executor->process({stream});
     ASSERT_TRUE(status.ok()) << status;
     EXPECT_EQ(stream->getCompleteTokenIds()->completeTokenIdsVec(0), (std::vector<int>{0, 1, 2, 3}));
-    EXPECT_TRUE(stream->hasError());
-    EXPECT_EQ(stream->statusInfo().code(), ErrorCode::NUMERICAL_NONFINITE);
+    EXPECT_TRUE(stream->stopped());
 }
 
 TEST_F(MtpExecutorTest, testForceDisableSpRunMixedBatchFailsClosed) {
