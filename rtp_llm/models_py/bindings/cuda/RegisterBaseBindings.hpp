5ec67cb1d22d5ff6f9eca2d1bafeac4b5491f2cc
fanxingpeng.fxp <fanxingpeng.fxp@alibaba-inc.com>
2026-08-27 21:17:02 +0800

    feat(ppu): add AOT fused SwiGLU MXFP4 kernel

--- rtp_llm/models_py/bindings/cuda/RegisterBaseBindings.hpp
+++ rtp_llm/models_py/bindings/cuda/RegisterBaseBindings.hpp
@@ -18,6 +18,9 @@
 #include "rtp_llm/models_py/bindings/cuda/DebugKernelOp.h"
 #include "rtp_llm/models_py/bindings/cuda/UserBuffersOp.h"
 #include "rtp_llm/models_py/bindings/cuda/FakeBalanceExpertOp.h"
+#ifdef USE_PPU
+#include "rtp_llm/models_py/bindings/cuda/PpuSiluMulMxfp4Op.h"
+#endif
 
 #include "rtp_llm/models_py/bindings/cuda/kernels/mla_quant_kernel.h"
 #include "rtp_llm/models_py/bindings/cuda/kernels/dsv4_persistent_topk.h"
@@ -66,6 +69,15 @@ void registerBasicCudaOps(py::module& rtp_ops_m) {
                   py::arg("output"),
                   py::arg("input"),
                   py::arg("cuda_stream") = 0);
+
+#ifdef USE_PPU
+    rtp_ops_m.def("ppu_silu_and_mul_post_quant_mxfp4",
+                  &rtp_llm::PpuSiluAndMulPostQuantMxfp4,
+                  "PPU fused SwiGLU and compact MXFP4 quantization",
+                  py::arg("gate_up"),
+                  py::arg("swiglu_limit"),
+                  py::arg("apply_swiglu_limit"));
+#endif
 
     rtp_ops_m.def("fused_qk_rmsnorm",
                   &FusedQKRMSNorm,
