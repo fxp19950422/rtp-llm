d00d6a6fd64f1c3846e023dbd11223f64642d981
fxp19950422 <fanxingpeng.fxp@alibaba-inc.com>
2026-09-23 14:10:59 +0800

    test: skip PPU bootstrap on CUDA

--- rtp_llm/platforms/__init__.py
+++ rtp_llm/platforms/__init__.py
@@ -9,11 +9,21 @@ from importlib import import_module
 _BUILTIN_PLATFORMS = ("rtp_llm.platforms.ppu",)
 
 
+def _uses_builtin_platforms():
+    from rtp_llm.device.device_type import DeviceType, get_device_type
+
+    return get_device_type() == DeviceType.Ppu
+
+
 def register_backend_hooks():
+    if not _uses_builtin_platforms():
+        return
     for name in _BUILTIN_PLATFORMS:
         import_module(name).register_backend_hooks()
 
 
 def register_modules(registry):
+    if not _uses_builtin_platforms():
+        return
     for name in _BUILTIN_PLATFORMS:
         import_module(name).register_modules(registry)
